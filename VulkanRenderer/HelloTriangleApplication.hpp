3f9ddcd3453b88dd09a4538fe0761b4c0f2fb796
Thomipon <38535540+Thomipon@users.noreply.github.com>
2025-04-16 14:42:23 +0200

    Clean up App class

--- VulkanRenderer/HelloTriangleApplication.hpp
+++ VulkanRenderer/HelloTriangleApplication.hpp
@@ -21,7 +21,6 @@ public:
     inline static std::filesystem::path modelPath{"../../VulkanRenderer/Meshes/Mesh.obj"}; // TODO: Awful file management in many ways. These should be in some asset system
     inline static std::filesystem::path texturePath{"../../VulkanRenderer/Textures/Texture.png"};
 private:
-    void initVulkan();
     void mainLoop();
 
     void initScene();
@@ -33,34 +32,4 @@ private:
     void handleCameraMovement(const InputEvent& keyEvent);
 
     Scene scene;
-
-    vk::raii::DescriptorSetLayout descriptorSetLayout{VK_NULL_HANDLE};
-    vk::raii::PipelineLayout pipelineLayout{VK_NULL_HANDLE};
-    vk::raii::Pipeline graphicsPipeline{VK_NULL_HANDLE};
-    void createGraphicsPipeline();
-
-    void recordCommandBuffer(const vk::CommandBuffer& commandBuffer, uint32_t imageIndex);
-
-    uint32_t currentFrame{0};
-    void drawFrame();
-
-    void updateUniformBuffer(uint32_t frameIndex);
-
-    std::vector<Buffer> uniformBuffers;
-    std::vector<void*> uniformBuffersMapped;
-    void createUniformBuffers();
-    
-    void createDescriptorSetLayout();
-
-    vk::raii::DescriptorPool descriptorPool{VK_NULL_HANDLE};
-    void createDescriptorPool();
-
-    std::vector<vk::raii::DescriptorSet> descriptorSets{};
-    void createDescriptorSets();
-
-    std::optional<Mesh> mesh;
-    void loadModel();
-
-    std::optional<TextureImage> texture;
-    void createTextureImage();
 };
